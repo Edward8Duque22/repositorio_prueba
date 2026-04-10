ac0163b3228740c559a71271ee122f714c738fd3
Edward8Duque22 <156961467+Edward8Duque22@users.noreply.github.com>
2026-04-09 19:27:15 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+const API = '/api?accion=';
+
+async function cargarProyectos() {
+    const res = await fetch(`${API}listar`);
+    return await res.json();
+}
+
+async function agregarProyecto() {
+    const foto = document.getElementById('foto').files[0];
+    if(!foto) return alert("Sube una imagen");
+
+    const reader = new FileReader();
+    reader.onload = async (e) => {
+        const nuevo = {
+            titulo: document.getElementById('titulo').value,
+            fecha:  document.getElementById('fecha').value,
+            img:    e.target.result,
+            repo:   document.getElementById('repo').value || '#',
+            live:   document.getElementById('live').value || '#',
+            desc:   document.getElementById('desc').value
+        };
+
+        const res = await fetch(`${API}guardar`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(nuevo)
+        });
+        
+        if(res.ok) {
+            alert("✓ Cambios guardados en MongoDB y auditados.");
+            location.reload();
+        }
+    };
+    reader.readAsDataURL(foto);
+}
+
+async function eliminarProyecto(id) {
+    if (confirm('¿Borrar? Esta acción se registrará en la auditoría.')) {
+        await fetch(`${API}eliminar&id=${id}`, { method: 'DELETE' });
+        location.reload();
+    }
+}
+
